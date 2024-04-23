b7aa3f9b3f4551c6b403a37986729dad5ce7526f
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2024-04-23 14:58:55 -0700

    file renamed

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -1,11 +1,10 @@
 async function main() {
-
   const Token = await ethers.getContractFactory("Token");
   const token = await Token.deploy();
 
   await token.deployed();
 
-  console.log("Token deployed to:", token.address);
+  console.log(`Token deployed to: ${token.address}`);
 }
 
 main().catch((error) => {
